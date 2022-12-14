80fdb678d183dceecf12b7e9b7afda68a0f5845d
Ranjith757 <ranjithmg757@gmail.com>
2022-12-14 11:39:06 +0530

    redux store

--- redux/src/redux/Message/message.action.js
+++ redux/src/redux/Message/message.action.js
@@ -1,9 +1,10 @@
-
-let gmAction = (hi) => {
-    return {}
+let GM = 'GM'
+let GN = 'GN'
+let gmAction = () => {
+    console.log("Second - action")
+    return { type: GM }
 }
-let gnAction = (hello) => {
-    return {}
+let gnAction = () => {
+    return { type: GN }
 }
-export { gmAction, gnAction }
- //named export
+export { gmAction, gnAction, GM, GN } 
